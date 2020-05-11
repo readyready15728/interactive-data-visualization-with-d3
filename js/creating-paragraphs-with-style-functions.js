d4c005a7e312dd4d3f15b0ba0d39093e4c49c894
Lynn Bradshaw <readyready15728@gmail.com>
2020-05-11 05:05:28 -0400

    Adding "creating paragraphs with style functions"

---
+++ js/creating-paragraphs-with-style-functions.js
@@ -0,0 +1,10 @@
+let dataset = [5, 10, 15, 20, 25];
+
+d3.select('body').selectAll('p')
+  .data(dataset)
+  .enter()
+  .append('p')
+  .text((d) => { return 'I can count up to ' + d; })
+  .style('color', (d) => {
+    return (d > 15) ? 'red' : 'black';
+  });
